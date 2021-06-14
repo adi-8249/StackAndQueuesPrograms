91bcf917ec74a88741db561253fc2c65cc7b9206
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-14 22:25:34 +0530

    [Anjaneya]Added:Pushing numbers in stack

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -7,6 +7,11 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queues programs");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
 }
---
+++ StackAndQueue/StackAndQueue/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    public class Stack
+    {
+        Node top;
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                top = node;
+            }
+            else
+            {
+                node.next = top;
+                top = node;
+            }
+            Console.WriteLine(node.data + " pushed into Stack");
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Node n = top;
+                while (n.next != null)
+                {
+                    Console.Write(n.data + " ");
+                    n = n.next;
+                }
+                Console.Write(n.data + "\n");
+            }
+        }
+
+
+    }
+}
